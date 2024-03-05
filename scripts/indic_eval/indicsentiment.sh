36408c236539a6d7b09b034f5049c6bbc7ecaecb
Shwu <shubham.kushwaha@nyunai.com>
2024-03-05 18:38:39 +0000

    indicsentiment

--- scripts/indic_eval/indicsentiment.sh
+++ scripts/indic_eval/indicsentiment.sh
@@ -1,48 +1,106 @@
+# BASE="sarvamai/OpenHathi-7B-Hi-v0.1-Base"
+# QUANTS=""
+# ADAPTED="ai4bharat/airavata"
+# RESULTS="results"
+
+# -------------------------------------------------------------
+# 
+# 
+#                       IndicSentiment
+# 
+# 
+# -------------------------------------------------------------
+
 export CUDA_VISIBLE_DEVICES=0
 
+echo "Evaluating IndicSentiment for each model..."
 
-model_name_or_path="sarvamai/OpenHathi-7B-Hi-v0.1-Base"
+IFS=','
 
-echo "evaluating openhathi base on indicsentiment ..."
+# ============================================================
+#                   Base Models
+# ============================================================
 
-# zero-shot
-python3 -m eval.indicsentiment.run_eval \
-    --ntrain 0 \
-    --save_dir "results/indicsentiment/openhathi-base-0shot" \
-    --model_name_or_path $model_name_or_path \
-    --tokenizer_name_or_path $model_name_or_path \
-    --eval_batch_size 8
+for model_path_or_name in $BASE; do
+    model_name=$(basename "${model_path_or_name}")
 
-# 5-shot
-python3 -m eval.indicsentiment.run_eval \
-    --ntrain 5 \
-    --save_dir "results/indicsentiment/openhathi-base-5shot" \
-    --model_name_or_path $model_name_or_path \
-    --tokenizer_name_or_path $model_name_or_path \
-    --eval_batch_size 4
+    echo "evaluating $model_name on IndicSentiment ..."
 
+    # zero-shot
+    python3 -m eval.indicsentiment.run_eval \
+        --ntrain 0 \
+        --save_dir "$RESULTS/indicsentiment/$model_name-0shot" \
+        --model_name_or_path $model_path_or_name \
+        --tokenizer_name_or_path $model_path_or_name \
+        --eval_batch_size 8
 
-model_name_or_path="ai4bharat/airavata"
-
-echo "evaluating airavata on indicsentiment ..."
-
-# zero-shot
-python3 -m eval.indicsentiment.run_eval \
-    --ntrain 0 \
-    --save_dir "results/indicsentiment/airavata-0shot" \
-    --model_name_or_path $model_name_or_path \
-    --tokenizer_name_or_path $model_name_or_path \
-    --eval_batch_size 8 \
-    --use_chat_format \
-    --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
+    # 5-shot
+    python3 -m eval.indicsentiment.run_eval \
+        --ntrain 5 \
+        --save_dir "$RESULTS/indicsentiment/$model_name-5shot" \
+        --model_name_or_path $model_path_or_name \
+        --tokenizer_name_or_path $model_path_or_name \
+        --eval_batch_size 4
+done
 
 
-# 5-shot
-python3 -m eval.indicsentiment.run_eval \
-    --ntrain 5 \
-    --save_dir "results/indicsentiment/airavata-5shot" \
-    --model_name_or_path $model_name_or_path \
-    --tokenizer_name_or_path $model_name_or_path \
-    --eval_batch_size 4 \
-    --use_chat_format \
-    --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
+# ============================================================
+#                   Quantized Models
+# ============================================================
+
+
+for model_path_or_name in $QUANTS; do
+    model_name=$(basename "${model_path_or_name}")
+
+    echo "evaluating $model_name on IndicSentiment ..."
+
+    # zero-shot
+    python3 -m eval.indicsentiment.run_eval \
+        --ntrain 0 \
+        --save_dir "$RESULTS/indicsentiment/$model_name-0shot" \
+        --model_name_or_path $model_path_or_name \
+        --tokenizer_name_or_path $model_path_or_name \
+        --awq \
+        --eval_batch_size 8
+
+    # 5-shot
+    python3 -m eval.indicsentiment.run_eval \
+        --ntrain 5 \
+        --save_dir "$RESULTS/indicsentiment/$model_name-5shot" \
+        --model_name_or_path $model_path_or_name \
+        --tokenizer_name_or_path $model_path_or_name \
+        --awq \
+        --eval_batch_size 4
+done
+
+
+# ============================================================
+#                   Adapted Models
+# ============================================================
+
+
+for model_path_or_name in $ADAPTED; do
+    model_name=$(basename "${model_path_or_name}")
+    echo "evaluating $model_name on IndicSentiment ..."
+
+    # zero-shot
+    python3 -m eval.indicsentiment.run_eval \
+        --ntrain 0 \
+        --save_dir "$RESULTS/indicsentiment/$model_name-0shot" \
+        --model_name_or_path $model_path_or_name \
+        --tokenizer_name_or_path $model_path_or_name \
+        --eval_batch_size 8 \
+        --use_chat_format \
+        --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
+
+    # 5-shot
+    python3 -m eval.indicsentiment.run_eval \
+        --ntrain 5 \
+        --save_dir "$RESULTS/indicsentiment/$model_name-5shot" \
+        --model_name_or_path $model_path_or_name \
+        --tokenizer_name_or_path $model_path_or_name \
+        --eval_batch_size 4 \
+        --use_chat_format \
+        --chat_formatting_function eval.templates.create_prompt_with_tulu_chat_format
+
+done
